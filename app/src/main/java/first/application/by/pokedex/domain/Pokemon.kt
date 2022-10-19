b644b2adf873fae549673da2f8e2e069913aaa62
luc-ads <lucaspereira88633@gmail.com>
2022-10-19 09:58:40 -0300

    Ajustes

--- app/src/main/java/first/application/by/pokedex/domain/Pokemon.kt
+++ app/src/main/java/first/application/by/pokedex/domain/Pokemon.kt
@@ -1,12 +1,15 @@
 package first.application.by.pokedex.domain
 
-class Pokemon (
+import java.util.*
+
+class Pokemon(
     val number: Int,
     val name: String,
     val types: List<PokemonType>
 
 ) {
-    val formattedName = name.capitalize()
+    val formattedName =
+        name.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
     val formattedNumber = number.toString().padStart(3, '0')
     val imagemUrl = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/$formattedNumber.png"
 }
--- app/src/main/java/first/application/by/pokedex/view/PokemonAdapter.kt
+++ app/src/main/java/first/application/by/pokedex/view/PokemonAdapter.kt
@@ -1,24 +1,21 @@
 package first.application.by.pokedex.view
 
 import android.annotation.SuppressLint
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import first.application.by.pokedex.R
 import first.application.by.pokedex.databinding.PokemonItemBinding
 import first.application.by.pokedex.domain.Pokemon
+import java.util.*
 
 class PokemonAdapter(
     private val items: List<Pokemon?>
 
 ) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
-    lateinit var binding: PokemonItemBinding
+    private lateinit var binding: PokemonItemBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
@@ -36,21 +33,25 @@ class PokemonAdapter(
     class ViewHolder(private val binding: PokemonItemBinding): RecyclerView.ViewHolder(binding.root) {
         @SuppressLint("SetTextI18n")
         fun bindView(item: Pokemon?) = with(itemView){
-            item?.let{
+            item?.let{ it ->
                 Glide.with(itemView.context).load(it.imagemUrl).into(binding.imageViewPokemon)
-                with(binding){
-                    pokeName.text= item.formattedName
-                    numeration.text= "Nº ${item.formattedNumber}"
-                    tvType01.text= item.types[0].name.capitalize()
+                with(binding) {
+                    pokeName.text = item.formattedName
+                    numeration.text = "Nº ${item.formattedNumber}"
+                    tvType01.text = item.types[0].name.replaceFirstChar {
+                        if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
+                    }
                     if (item.types[0].name == "fire") {
                         tvType01.setTextColor(ContextCompat.getColor(context, R.color.purple_700))
                     }
                     if (item.types.size > 1) {
-                        tvType02.visibility= View.VISIBLE
-                        tvType02.text= item.types[1].name.capitalize()
+                        tvType02.visibility = View.VISIBLE
+                        tvType02.text = item.types[1].name.replaceFirstChar {
+                            if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
+                        }
 
                     } else {
-                        tvType02.visibility= View.GONE
+                        tvType02.visibility = View.GONE
                     }
                 }
             }
